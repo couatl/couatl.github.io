372cd3cfdb5d9a2542ff84977d9c0947900a9329
Svoykina N <nadya.svojkina@gmail.com>
2018-09-02 04:42:11 +0300

    Fisrt screen of cv

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,39 @@
 <template>
   <div class="main">
     <section class="logo">
+      <app-header/>
       <div>
         <github/>
-        <h1 class="logo__title-text">
-          Hi! I'm
-        </h1>
-        <h1 class="logo__title" data-buzz="Nadya">
-          Nadya
-        </h1>
-        <h2 class="logo__title-description">
-          Web developer
-        </h2>
-        <!--<h1 class="title">-->
-        <!--Hi! I'm Nadya-->
-        <!--</h1>-->
-        <!--<div class="logo__text">-->
-        <!--<h2 class="subtitle">-->
-        <!--Web developer-->
-        <!--</h2>-->
-        <!--</div>-->
-        <img src="title-2.png" class="photo"/>
+        <app-title/>
+        <img src="landing.png" class="photo"/>
+        <div style="position: relative">
+          <div class="yellow-circle"/>
+        </div>
+        <div class="landing__down">
+          <div class="landing__down-text">
+            About
+          </div>
+          <img src="up.png" class="landing__down-icon"/>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <script>
+
+  // TODO: При ховере на руку + about -> баунсинг руки
+  // TODO: багфикс ховера руки
+
   import Github from '~/components/Github.vue'
+  import AppHeader from '~/components/AppHeader.vue'
+  import AppTitle from '~/components/AppTitle.vue'
 
   export default {
     components: {
-      Github
+      Github,
+      AppHeader,
+      AppTitle
     }
   }
 </script>
@@ -44,87 +46,65 @@
     padding: 1em;
     align-items: flex-end;
     justify-content: center;
-    /*background-color: gray;*/
-    /*background: url('../static/banner-background.png') no-repeat top center;*/
-    /*background-size: cover;*/
     box-shadow: inset 0 -1em #732a2f;
   }
 
-  .logo__title-text {
-    display: inline;
-  }
-
-  .logo__title {
-    display: inline-block;
-    position: relative;
-    color: inherit;
-    text-decoration: none;
-    transition: all 250ms ease;
-    transform: translatez(0);
-    perspective: 50px;
-  }
-
-  .logo__title:before,
-  .logo__title:after {
-    content: attr(data-buzz);
-    position: absolute;
-    z-index: -1;
-    opacity: 0.5;
-    transition: all 150ms ease;
-  }
-
-  .logo__title:before {
-    color: #ad1f2d;
-    top: 2px;
-    left: 2px;
-  }
-
-  .logo__title:after {
-    color: #ffa522;
-    bottom: 2px;
-    right: 2px;
-  }
-
-  .logo__title:hover:before {
-    transform: rotatex(-15deg) rotatey(5deg) scale(1.25);
-  }
-
-  .logo__title:hover:after {
-    transform: rotatey(-5deg) rotatex(15deg) scale(1.25);
-  }
-
-  .logo__title-description {
-
-  }
-
-  .logo__text {
-    background: #732a2fc2;
-    width: 35em;
-    padding: 1em;
-    box-shadow: 1em 1em #b8641f9c;
-  }
-
-  .title {
-    display: block;
-    font-weight: 700;
-    color: #b8641f9c;
-    font-family: "Moon", sans-serif;
-    font-size: 5em;
-    /*text-shadow: 0.1em 0.1em rgba(115, 42, 47, .76);*/
-    letter-spacing: 7px;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: white;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
   .photo {
-    height: 500px;
     display: block;
+    max-width: 100%;
+    width: 1200px;
+    margin-left: -2em;
   }
+
+  .yellow-circle {
+    width: 700px;
+    height: 700px;
+    border: solid 350px #ffa522;
+    border-radius: 100%;
+    z-index: -1;
+    opacity: 0.2;
+    position: absolute;
+    bottom: 15em;
+    filter: blur(5px);
+  }
+
+  .landing__down {
+    display: flex;
+    position: absolute;
+    right: 1em;
+    bottom: 1.5em;
+    flex-direction: column;
+    cursor: pointer;
+    z-index: 100;
+  }
+
+  .landing__down:hover {
+    color: darkred;
+  }
+
+  .landing__down-icon {
+    width: 50px;
+    height: 50px;
+    transform: rotate(180deg);
+    transition: all 250ms ease;
+  }
+
+  .landing__down-icon:hover {
+    transform:  rotate(180deg) translateY(-10px);
+  }
+
+  .landing__down-text {
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    font-weight: 300;
+    font-size: 0.7em;
+    writing-mode: vertical-rl;
+    text-orientation: inherit;
+    margin-bottom: 10px;
+    line-height: 50px;
+    -webkit-transform: rotate(180deg);
+    transition: all 250ms ease;
+  }
+
 </style>
 
