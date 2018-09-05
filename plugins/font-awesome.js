fdce0b36b94ff0cc26eaa5b6dab8c7ecb92e5a6f
Svoykina N <nadya.svojkina@gmail.com>
2018-09-06 01:54:11 +0300

    Added more icons

--- plugins/font-awesome.js
+++ plugins/font-awesome.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faDownload, faGraduationCap, faGlobe, faCode, faDesktop } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {
+  faDownload, faGraduationCap, faGlobe, faCode,
+  faDesktop, faMapSigns, faPhone, faAddressCard, faBriefcase, faMapMarkerAlt
+} from '@fortawesome/free-solid-svg-icons'
+import {faGithub, faVk, faFacebookF, faTelegram} from '@fortawesome/free-brands-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
-library.add(faDownload, faGraduationCap, faGlobe, faCode, faDesktop);
+library.add(faDownload, faGraduationCap, faGlobe, faCode, faDesktop, faMapSigns, faPhone, faAddressCard, faBriefcase, faMapMarkerAlt);
+
+library.add(faGithub, faVk, faFacebookF, faTelegram);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
