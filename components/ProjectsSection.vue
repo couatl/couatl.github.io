182d1b926403258b987a511137a7a2bfab65189b
Svoykina N <nadya.svojkina@gmail.com>
2018-09-06 04:54:58 +0300

    + projects

---
+++ components/ProjectsSection.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <section id="projects" class="projects">
+    <div class="projects__title">Projects</div>
+    <div class="projects-grid">
+      <div id="apoj-button" class="projects-grid__item">
+        <div class="projects-grid__item_info-mobile">
+          <div class="projects-grid__item_info-mobile_title">Apoj</div>
+          <div class="info_label frontend">Frontend</div>
+          <div class="projects-grid__item_info-mobile_description">SPA Browser Game</div>
+        </div>
+        <img class="projects-grid__item_image" src="img/apoj.png" alt>
+        <div class="projects-grid__item_info">
+          <div class="info__text fadeInUp">
+            <div class="info__text_title">Apoj</div>
+            <div class="info__text_description">SPA Browser Game</div>
+          </div>
+        </div>
+      </div>
+
+      <div id="forum-button" class="projects-grid__item">
+        <div class="projects-grid__item_info-mobile">
+          <div class="projects-grid__item_info-mobile_title">Forum</div>
+          <div class="info_label backend">Backend</div>
+          <div class="projects-grid__item_info-mobile_description">REST Api to Forums</div>
+        </div>
+        <img class="projects-grid__item_image" src="img/forum.png" alt>
+        <div class="projects-grid__item_info">
+          <div class="info__text">
+            <div class="info__text_title">Forum</div>
+            <div class="info__text_description">REST Api to Forums</div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </section>
+</template>
+<style>
+
+</style>
