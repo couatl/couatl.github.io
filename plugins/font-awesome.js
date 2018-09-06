3b0db5da36d27c021169bc236f7dccebcde41bd9
Svoykina N <nadya.svojkina@gmail.com>
2018-09-06 22:15:32 +0300

    + fonts

--- plugins/font-awesome.js
+++ plugins/font-awesome.js
@@ -3,13 +3,15 @@ import Vue from 'vue'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {
   faDownload, faGraduationCap, faGlobe, faCode,
-  faDesktop, faMapSigns, faPhone, faAddressCard, faBriefcase, faMapMarkerAlt, faCertificate, faCalendarAlt, faFilePdf
+  faDesktop, faMapSigns, faPhone, faAddressCard, faBriefcase, faMapMarkerAlt, faCertificate, faCalendarAlt, faFilePdf,
+  faBoxes, faFileCode
 } from '@fortawesome/free-solid-svg-icons'
-import {faGithub, faVk, faFacebookF, faTelegram} from '@fortawesome/free-brands-svg-icons'
+import {faGithub, faVk, faFacebookF, faTelegram, faJsSquare, faReact, faVuejs, faCss3, faGit, faDocker, faNodeJs} from '@fortawesome/free-brands-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
 library.add(faDownload, faGraduationCap, faGlobe, faCode, faDesktop, faMapSigns, faFilePdf,
-  faPhone, faAddressCard, faBriefcase, faMapMarkerAlt, faCertificate, faCalendarAlt);
+  faPhone, faAddressCard, faBriefcase, faMapMarkerAlt, faCertificate, faCalendarAlt, faFileCode,
+  faBoxes, faJsSquare, faReact, faVuejs, faCss3, faGit, faDocker, faNodeJs);
 
 library.add(faGithub, faVk, faFacebookF, faTelegram);
 
