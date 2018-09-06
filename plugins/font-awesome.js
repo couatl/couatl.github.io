7a4c093b987c9851b92763a743ac4ff29e928242
Svoykina N <nadya.svojkina@gmail.com>
2018-09-06 04:55:22 +0300

    more icons

--- plugins/font-awesome.js
+++ plugins/font-awesome.js
@@ -3,12 +3,13 @@ import Vue from 'vue'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {
   faDownload, faGraduationCap, faGlobe, faCode,
-  faDesktop, faMapSigns, faPhone, faAddressCard, faBriefcase, faMapMarkerAlt
+  faDesktop, faMapSigns, faPhone, faAddressCard, faBriefcase, faMapMarkerAlt, faCertificate, faCalendarAlt, faFilePdf
 } from '@fortawesome/free-solid-svg-icons'
 import {faGithub, faVk, faFacebookF, faTelegram} from '@fortawesome/free-brands-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
-library.add(faDownload, faGraduationCap, faGlobe, faCode, faDesktop, faMapSigns, faPhone, faAddressCard, faBriefcase, faMapMarkerAlt);
+library.add(faDownload, faGraduationCap, faGlobe, faCode, faDesktop, faMapSigns, faFilePdf,
+  faPhone, faAddressCard, faBriefcase, faMapMarkerAlt, faCertificate, faCalendarAlt);
 
 library.add(faGithub, faVk, faFacebookF, faTelegram);
 
