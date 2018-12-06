9017b952243926d695c536d5777ec1f04b59cd69
Svoykina N <nadya.svojkina@gmail.com>
2018-12-06 21:30:39 +0300

    + insta icon

--- plugins/font-awesome.js
+++ plugins/font-awesome.js
@@ -6,14 +6,14 @@ import {
   faDesktop, faMapSigns, faPhone, faAddressCard, faBriefcase, faMapMarkerAlt, faCertificate, faCalendarAlt, faFilePdf,
   faBoxes, faFileCode
 } from '@fortawesome/free-solid-svg-icons'
-import {faGithub, faVk, faFacebookF, faTelegram, faJsSquare, faReact, faVuejs, faCss3, faGit, faDocker, faNodeJs} from '@fortawesome/free-brands-svg-icons'
+import {faGithub, faVk, faFacebookF, faTelegram, faJsSquare, faReact, faVuejs, faCss3, faGit, faDocker, faNodeJs, faInstagram} from '@fortawesome/free-brands-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
 library.add(faDownload, faGraduationCap, faGlobe, faCode, faDesktop, faMapSigns, faFilePdf,
   faPhone, faAddressCard, faBriefcase, faMapMarkerAlt, faCertificate, faCalendarAlt, faFileCode,
   faBoxes, faJsSquare, faReact, faVuejs, faCss3, faGit, faDocker, faNodeJs);
 
-library.add(faGithub, faVk, faFacebookF, faTelegram);
+library.add(faGithub, faVk, faFacebookF, faTelegram, faInstagram);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
