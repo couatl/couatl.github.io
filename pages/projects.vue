ce63aa9fbdbbba7295fbf70945f6bd870678323b
Svoykina N <nadya.svojkina@gmail.com>
2018-12-06 21:30:59 +0300

    + projects page

---
+++ pages/projects.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="root">
+    <projects-section/>
+    <div class="projects__triangle"/>
+    <div class="projects__triangle_right"/>
+    <custom-footer/>
+  </div>
+</template>
+<script>
+  import ProjectsSection from '~/components/ProjectsSection.vue'
+  import CustomFooter from '~/components/CustomFooter.vue'
+
+  export default {
+    components: {
+      ProjectsSection,
+      CustomFooter
+    }
+  }
+</script>
+
+<style>
+  .projects__triangle {
+    position: absolute;
+    z-index: -1;
+    width: 0;
+    height: 0;
+    border: transparent solid;
+    border-width: 5em 15em 0 0;
+    border-top-color: #eee;
+    border-right-color: rgba(0, 0, 255, 0);
+  }
+
+  .projects__triangle_right {
+    position: absolute;
+    z-index: -1;
+    width: 0;
+    height: 0;
+    border: transparent solid;
+    right: 0;
+    border-width: 5em 0 0 15em;
+    border-top-color: #eee;
+    border-right-color: rgba(0, 0, 255, 0);
+  }
+</style>
