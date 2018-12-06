a59a99e1d71eb7978f8825a37a435dc106217523
Svoykina N <nadya.svojkina@gmail.com>
2018-12-06 21:31:55 +0300

    Added data to projects section

--- components/ProjectsSection.vue
+++ components/ProjectsSection.vue
@@ -17,9 +17,56 @@
     data: function () {
       return {
         projectsList: [
-          {id: 0, title: 'Apoj', tags: 'Frontend', description: 'SPA Browser Game', img: 'apoj.png', tagColor: '#b8641f'},
-          {id: 1, title: 'Forum', tags: 'Backend', description: 'REST Api to Forums', img: 'forum.png', tagColor: '#5f9ea0'},
-          {id: 2, title: 'Mort', tags: 'Desktop', description: 'Computer game', img: 'mort.png', tagColor: '#9aa5ea'}
+          {
+            id: 0,
+            title: 'Apoj',
+            tags: 'Frontend',
+            description: '— single page application for browser game',
+            img: 'apoj.png',
+            tagColor: '#b8641f',
+            features: [{
+              id: 0, data: "Working offline (Service Worker)"
+            },
+              {
+                id: 1, data: "Services for working with Web Audio Api"
+              }, {id: 2, data: "VPS deploy supporting HTTPS, HTTP/2"}, {id: 3, data: "Web Sockets"}, {
+                id: 4,
+                data: "CI with autodeploy on VPS"
+              }],
+            technologies: ["Javascript", "Typescript", "ES6", "Webpack", "SASS", "BEM",
+              "Web Audio Api", "Websockets", "Routing", "Service worker", "nginx", "HTTP/2", "HTTPS"]
+          },
+          {
+            id: 1,
+            title: 'Forum',
+            tags: 'Backend',
+            description: '— developed REST Api to forum according to swagger docs',
+            img: 'forum.png',
+            tagColor: '#5f9ea0', features: [{
+              id: 0, data: "Used PostgreSQL, Golang pq driver"
+            },
+              {
+                id: 1, data: "Docker container"
+              },
+              {
+                id: 2, data: "Performance test ended with 1269 RPS"
+              }],
+            technologies: ["Go", "PostgreSQL", "Docker"]
+          },
+          {
+            id: 2,
+            title: 'Mort',
+            tags: 'Desktop',
+            description: '— original desktop game',
+            img: 'mort.png',
+            tagColor: '#9aa5ea', features: [{
+              id: 0, data: "Inspired by Terry Pratchett's Discworld novels"
+            },
+              {
+                id: 1, data: "Crossplatform (Unix, Windows)"
+              }],
+          technologies: ["C++", "Qt", "OpenGL", "XML"]
+          }
         ]
       }
     },
@@ -31,8 +78,9 @@
 
 <style>
   .projects {
-    padding-top: 2em;
+    padding-top: 6em;
     padding-bottom: 2em;
+    font-size: 1.1em;
   }
 
   .projects__title {
